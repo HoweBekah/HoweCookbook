e06b69c83a307ab657cc49f90d5b0f6cd3fff71e
HoweBekah <how18019@byui.edu>
2019-11-29 23:23:41 -0700

    ajax call maybe

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const { Pool } = require("pg");
 const ajax = require("ajax");
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var xhr = new XMLHttpRequest();
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const connectionString = process.env.DATABASE_URL;
@@ -10,6 +12,22 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.get("/", (req, res) => res.render("pages/index"));
+
+xhr.open("GET", "/getRecipes");
+
+xhr.responseType = "json";
+
+xhr.send();
+
+// the response is {"message": "Hello, world!"}
+xhr.onload = function(BeveragesJSON) {
+  let results = "";
+  for (var i = 0; i < result.rows.length; i++) {
+    results = BeveragesJSON.result.rows[i].recipe_name;
+
+    $("#ulgarbage").append(results);
+  }
+};
 
 // $.ajax({
 //   url: "/getRecipes",
@@ -23,42 +41,42 @@ app.get("/", (req, res) => res.render("pages/index"));
 //   }
 // });
 
-app
-  .get("/getRecipes", function(req, res) {
-    const pool = new Pool({ connectionString: connectionString });
-    var sql = "Select * FROM recipes WHERE category = 'Beverages'";
-    pool.query(sql, function(err, result) {
-      if (err) {
-        console.log("Error in query: ");
-        console.log(err);
-      }
+// app
+//   .get("/getRecipes", function(req, res) {
+//     const pool = new Pool({ connectionString: connectionString });
+//     var sql = "Select * FROM recipes WHERE category = 'Beverages'";
+//     pool.query(sql, function(err, result) {
+//       if (err) {
+//         console.log("Error in query: ");
+//         console.log(err);
+//       }
 
-      //console.log(result.rows);
-      //const results = JSON.stringify(result.rows);
-      //console.log(results);
-      let results = "";
-      for (var i = 0; i < result.rows.length; i++) {
-        let jSONObj = JSON.stringify(result.rows[i].recipe_name);
-        console.log(jSONObj);
+//       //console.log(result.rows);
+//       //const results = JSON.stringify(result.rows);
+//       //console.log(results);
+//       let results = "";
+//       for (var i = 0; i < result.rows.length; i++) {
+//         let jSONObj = JSON.stringify(result.rows[i].recipe_name);
+//         console.log(jSONObj);
 
-        results += trimChar(jSONObj, '"') + " ";
-      }
-      //results += "</ul>";
+//         results += trimChar(jSONObj, '"') + " ";
+//       }
+//       //results += "</ul>";
 
-      res.render("pages/testresults", {
-        result: results
-      });
-    });
-    function trimChar(string, charToRemove) {
-      while (string.charAt(0) == charToRemove) {
-        string = string.substring(1);
-      }
+//       res.render("pages/testresults", {
+//         result: results
+//       });
+//     });
+//     function trimChar(string, charToRemove) {
+//       while (string.charAt(0) == charToRemove) {
+//         string = string.substring(1);
+//       }
 
-      while (string.charAt(string.length - 1) == charToRemove) {
-        string = string.substring(0, string.length - 1);
-      }
+//       while (string.charAt(string.length - 1) == charToRemove) {
+//         string = string.substring(0, string.length - 1);
+//       }
 
-      return string;
-    }
-  })
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+//       return string;
+//     }
+// })
+// .listen(PORT, () => console.log(`Listening on ${PORT}`));
