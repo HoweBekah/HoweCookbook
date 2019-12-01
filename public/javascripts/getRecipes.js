db1e0b4a04d7a7fbeea660a80d080965b737332b
HoweBekah <how18019@byui.edu>
2019-11-30 22:31:58 -0700

    the most recent business that doesn't work yet

--- public/javascripts/getRecipes.js
+++ public/javascripts/getRecipes.js
@@ -4,78 +4,73 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhr = new XMLHttpRequest();
 var app = express();
 function tryit() {
-  $.ajax({
-    url: "/getRecipes",
-    method: "GET"
-  }).then(function(BeveragesJSON) {
-    let results = "";
-    for (var i = 0; i < result.rows.length; i++) {
-      results = BeveragesJSON.result.rows[i].recipe_name;
+  //   $.ajax({
+  //     url: "/getRecipes",
+  //     method: "GET"
+  //   }).then(function(BeveragesJSON) {
+  //     let results = "";
+  //     for (var i = 0; i < result.rows.length; i++) {
+  //       results = BeveragesJSON.result.rows[i].recipe_name;
 
-      $("#ulgarbage").append(results);
-    }
-  });
+  //       $("#ulgarbage").append(results);
+  //     }
+  //   });
+
+  xhr.open("GET", "/getRecipes");
+
+  xhr.responseType = "json";
+
+  xhr.send();
+
+  // the response is {"message": "Hello, world!"}
+  function search() {
+    //alert("Hello Korbin!!!");
+
+    var searchTerm = document.getElementById("txtSearch").value;
+
+    console.log(searchTerm);
+
+    var htmlapi = "http://www.omdbapi.com/";
+    var params = "?apikey=964cfd45&s=" + searchTerm;
+
+    var wholeURL = htmlapi + params;
+
+    var http = new XMLHttpRequest();
+
+    http.onload = function() {
+      if (http.readyState == 4 && http.status == 200) {
+        console.log("We have data!");
+        console.log(http.response);
+
+        var jsonData = JSON.parse(http.response);
+        var ourTable = document.getElementById("table");
+
+        ourTable.innerHTML =
+          "<tr>" + "<th>Title</th>" + "<th>MORE CLICKS!!!</th>" + "</tr>";
+
+        for (let i = 0; i < jsonData.Search.length; i++) {
+          var title = jsonData.Search[i].Title;
+          var id = jsonData.Search[i].imdbID;
+
+          // new row
+          var newRow = ourTable.insertRow(-1);
+
+          // new cell 1
+          var cell = newRow.insertCell(-1);
+          cell.innerHTML = title;
+
+          // new cell 2
+          var cell2 = newRow.insertCell(-1);
+          cell2.innerHTML =
+            // v--- LOOK HOW NICE THIS IS!! ---v
+            `<button onclick="foo('${id}')">Details</button>`;
+        }
+      } else {
+        console.log("nothing yet ...");
+      }
+    };
+
+    http.open("GET", wholeURL);
+    http.send();
+  }
 }
-
-// xhr.open("GET", "/getRecipes");
-
-// xhr.responseType = "json";
-
-// xhr.send();
-
-// // the response is {"message": "Hello, world!"}
-// function search() {
-//     //alert("Hello Korbin!!!");
-
-//     var searchTerm = document.getElementById("txtSearch").value;
-
-//     console.log(searchTerm);
-
-//     var htmlapi = "http://www.omdbapi.com/";
-//     var params = "?apikey=964cfd45&s=" + searchTerm;
-
-//     var wholeURL = htmlapi + params;
-
-//     var http = new XMLHttpRequest();
-
-//     http.onload = function() {
-//         if (http.readyState == 4 && http.status == 200) {
-
-//             console.log("We have data!");
-//             console.log(http.response);
-
-//             var jsonData = JSON.parse(http.response);
-//             var ourTable = document.getElementById('table');
-
-//             ourTable.innerHTML =
-//             '<tr>' +
-//                 '<th>Title</th>' +
-//                 '<th>MORE CLICKS!!!</th>' +
-//             '</tr>';
-
-//             for (let i = 0; i < jsonData.Search.length; i++) {
-//                 var title = jsonData.Search[i].Title;
-//                 var id = jsonData.Search[i].imdbID;
-
-//                 // new row
-//                 var newRow = ourTable.insertRow(-1);
-
-//                 // new cell 1
-//                 var cell = newRow.insertCell(-1);
-//                 cell.innerHTML = title;
-
-//                 // new cell 2
-//                 var cell2 = newRow.insertCell(-1);
-//                 cell2.innerHTML =
-//                 // v--- LOOK HOW NICE THIS IS!! ---v
-//                 `<button onclick="foo('${id}')">Details</button>`;
-//             }
-
-//         } else {
-//             console.log("nothing yet ...");
-//         }
-//     };
-
-//     http.open('GET', wholeURL);
-//     http.send();
-// }
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const path = require("path");
-const { Pool } = require("pg");
-const ajax = require("ajax");
+//const { Pool } = require("pg");
+// const ajax = require("ajax");
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var xhr = new XMLHttpRequest();
+const xhr = new XMLHttpRequest();
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const connectionString = process.env.DATABASE_URL;
@@ -12,54 +12,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.get("/", (req, res) => res.render("pages/index"));
-
-xhr.open("GET", "/getRecipes");
-
-xhr.responseType = "json";
-
-xhr.send();
-
-// the response is {"message": "Hello, world!"}
-xhr.onload = function() {
-  // let results = "";
-  // for (var i = 0; i < result.rows.length; i++) {
-  //   results = BeveragesJSON.result.rows[i].recipe_name;
-
-  //   $("#ulgarbage").append(results);}
-  if (xhr.readyState == 4 && xhr.status == 200) {
-    console.log("We have data!");
-    console.log(xhr.response);
-
-    //var jsonData = JSON.parse(http.response);
-    // var ourTable = document.getElementById('table');
-
-    // ourTable.innerHTML =
-    // '<tr>' +
-    //     '<th>Title</th>' +
-    //     '<th>MORE CLICKS!!!</th>' +
-    // '</tr>';
-
-    // for (let i = 0; i < jsonData.Search.length; i++) {
-    //     var title = jsonData.Search[i].Title;
-    //     var id = jsonData.Search[i].imdbID;
-
-    //     // new row
-    //     var newRow = ourTable.insertRow(-1);
-
-    //     // new cell 1
-    //     var cell = newRow.insertCell(-1);
-    //     cell.innerHTML = title;
-
-    //     // new cell 2
-    //     var cell2 = newRow.insertCell(-1);
-    //     cell2.innerHTML =
-    //     // v--- LOOK HOW NICE THIS IS!! ---v
-    //     `<button onclick="foo('${id}')">Details</button>`;
-    // }
-  } else {
-    console.log("nothing yet ...");
-  }
-};
 
 // $.ajax({
 //   url: "/getRecipes",
@@ -73,42 +25,90 @@ xhr.onload = function() {
 //   }
 // });
 
-// app
-//   .get("/getRecipes", function(req, res) {
-//     const pool = new Pool({ connectionString: connectionString });
-//     var sql = "Select * FROM recipes WHERE category = 'Beverages'";
-//     pool.query(sql, function(err, result) {
-//       if (err) {
-//         console.log("Error in query: ");
-//         console.log(err);
-//       }
+app
+  .get("/getRecipes", function(req, res) {
+    // const pool = new Pool({ connectionString: connectionString });
+    // var sql = "Select * FROM recipes WHERE category = 'Beverages'";
+    // pool.query(sql, function(err, result) {
+    xhr.open("GET", connectionString);
 
-//       //console.log(result.rows);
-//       //const results = JSON.stringify(result.rows);
-//       //console.log(results);
-//       let results = "";
-//       for (var i = 0; i < result.rows.length; i++) {
-//         let jSONObj = JSON.stringify(result.rows[i].recipe_name);
-//         console.log(jSONObj);
+    xhr.responseType = "json";
 
-//         results += trimChar(jSONObj, '"') + " ";
-//       }
-//       //results += "</ul>";
+    xhr.send();
 
-//       res.render("pages/testresults", {
-//         result: results
-//       });
-//     });
-//     function trimChar(string, charToRemove) {
-//       while (string.charAt(0) == charToRemove) {
-//         string = string.substring(1);
-//       }
+    // the response is {"message": "Hello, world!"}
+    xhr.onload = function(xhr) {
+      // let results = "";
+      // for (var i = 0; i < result.rows.length; i++) {
+      //   results = BeveragesJSON.result.rows[i].recipe_name;
 
-//       while (string.charAt(string.length - 1) == charToRemove) {
-//         string = string.substring(0, string.length - 1);
-//       }
+      //   $("#ulgarbage").append(results);}
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        console.log("We have data!");
+        console.log(xhr.response);
 
-//       return string;
-//     }
-// })
-// .listen(PORT, () => console.log(`Listening on ${PORT}`));
+        //var jsonData = JSON.parse(http.response);
+        // var ourTable = document.getElementById('table');
+
+        // ourTable.innerHTML =
+        // '<tr>' +
+        //     '<th>Title</th>' +
+        //     '<th>MORE CLICKS!!!</th>' +
+        // '</tr>';
+
+        // for (let i = 0; i < jsonData.Search.length; i++) {
+        //     var title = jsonData.Search[i].Title;
+        //     var id = jsonData.Search[i].imdbID;
+
+        //     // new row
+        //     var newRow = ourTable.insertRow(-1);
+
+        //     // new cell 1
+        //     var cell = newRow.insertCell(-1);
+        //     cell.innerHTML = title;
+
+        //     // new cell 2
+        //     var cell2 = newRow.insertCell(-1);
+        //     cell2.innerHTML =
+        //     // v--- LOOK HOW NICE THIS IS!! ---v
+        //     `<button onclick="foo('${id}')">Details</button>`;
+        // }
+      } else {
+        console.log("nothing yet ...");
+      }
+    };
+    //   if (err) {
+    //     console.log("Error in query: ");
+    //     console.log(err);
+    //   }
+
+    //   //console.log(result.rows);
+    //   //const results = JSON.stringify(result.rows);
+    //   //console.log(results);
+    //   let results = "";
+    //   for (var i = 0; i < result.rows.length; i++) {
+    //     let jSONObj = JSON.stringify(result.rows[i].recipe_name);
+    //     console.log(jSONObj);
+
+    //     results += trimChar(jSONObj, '"') + " ";
+    //   }
+    //   //results += "</ul>";
+
+    //   res.render("pages/testresults", {
+    //     result: results
+    //   });
+    // });
+    // function trimChar(string, charToRemove) {
+    //   while (string.charAt(0) == charToRemove) {
+    //     string = string.substring(1);
+    //   }
+
+    //   while (string.charAt(string.length - 1) == charToRemove) {
+    //     string = string.substring(0, string.length - 1);
+    //   }
+
+    //   return string;
+    // }
+    // });
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
